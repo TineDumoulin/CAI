56c2568fa0dec53898179e7d81cc964d66267f7b
TineDumoulin <tinedumoulin@gmail.com>
2019-03-19 18:11:10 +0100

    w/o error message, wip

--- langid.py
+++ langid.py
@@ -15,22 +15,17 @@ y = df['language']
 # print(X[:10])
 # print(y[:10])
 # print(X.shape)
-print(y.shape)
+# print(y.shape)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=44)
-# print(type(X_train), type(y_train))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=44)
 
-cv = CountVectorizer(analyzer='ngrams')
+cv = CountVectorizer()
 knn = KNeighborsClassifier()
 
 pipe = Pipeline([("cv", cv), ("knn", knn)])
-parameters = {'cv__ngram_range': [(1,1), (2,2), (3,3)], 'knn__n_neighbors': [3, 5, 7], 'knn__metric': ['manhattan', 'euclidian', 'chebyshev']} # without taking into account CV, the experiment will run 3*3*2=18 times
+parameters = {'cv__ngram_range': [(1,1), (1,2), (2,2)], 'knn__n_neighbors': [3, 5, 7], 'knn__metric': ['manhattan', 'euclidian', 'chebyshev']} # without taking into account CV, the experiment will run 3*3*3=27 times
 
 GSCV = GridSearchCV(pipe, parameters, iid=False, cv=10, return_train_score=False)
 GSCV.fit(X_train, y_train)
 print("Best parameter (CV score=%0.3f):" % GSCV.best_score_)
 print(GSCV.best_params_)
-
-# I keep getting this error:
-# ValueError: ngrams is not a valid tokenization scheme/analyzer
-# and I've tried looking up how to fix it, but it's not working.
