34929a857c17d360b110286a61eb6708949c0489
TineDumoulin <tinedumoulin@gmail.com>
2019-03-19 19:25:54 +0100

    wip

--- langid.py
+++ langid.py
@@ -6,7 +6,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 
-df = pd.read_csv('langid.csv', header=0)
+df =pd.read_csv('langid.csv', header=0)
 X = df['doc']
 y = df['language']
 
@@ -14,18 +14,20 @@ y = df['language']
 # print(type(y))
 # print(X[:10])
 # print(y[:10])
-# print(X.shape)
-# print(y.shape)
+# print(len(X))
+# print(len(y))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=44)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=44, stratify=y)
+
 
 cv = CountVectorizer()
 knn = KNeighborsClassifier()
 
-pipe = Pipeline([("cv", cv), ("knn", knn)])
-parameters = {'cv__ngram_range': [(1,1), (1,2), (2,2)], 'knn__n_neighbors': [3, 5, 7], 'knn__metric': ['manhattan', 'euclidian', 'chebyshev']} # without taking into account CV, the experiment will run 3*3*3=27 times
+pipe = Pipeline(steps=[('cv', cv), ('knn', knn)])
+parameters = {'cv__ngram_range': [(1,1), (1,2), (2,2)], 'knn__n_neighbors': [3, 5, 7], 'knn__metric': ['manhattan', 'cosine']} # without taking into account CV, the experiment will run 3*3*3=27 times
+# wasn't working with euclidian distancefunction, why?
 
-GSCV = GridSearchCV(pipe, parameters, iid=False, cv=10, return_train_score=False)
+GSCV = GridSearchCV(pipe, parameters, iid=False, cv=2, return_train_score=False, verbose=10)
 GSCV.fit(X_train, y_train)
 print("Best parameter (CV score=%0.3f):" % GSCV.best_score_)
 print(GSCV.best_params_)
