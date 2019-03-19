78c8ba477e040202634be45cda6e1f840d736339
TineDumoulin <tinedumoulin@gmail.com>
2019-03-19 20:42:21 +0100

    final version up until part 5

--- langid.py
+++ langid.py
@@ -18,7 +18,7 @@ knn = KNeighborsClassifier()
 
 pipe = Pipeline(steps=[('cv', cv), ('knn', knn)])
 parameters = {'cv__ngram_range': [(1,1), (1,2), (2,2)], 'knn__n_neighbors': [3, 5, 7], 'knn__metric': ['manhattan', 'cosine']} 
-# without taking into account CV, the experiment will run 3*3*3=27 times
+# without taking into account crossval, the experiment will run 3*3*3=27 times
 # wasn't working with euclidian distance function, why?
 
 GSCV = GridSearchCV(pipe, parameters, iid=False, cv=10, return_train_score=False, verbose=10)
