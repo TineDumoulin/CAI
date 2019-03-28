a15ec87a4e329e5e719fb336eeb9cb7c41029a05
TineDumoulin <tinedumoulin@gmail.com>
2019-03-28 10:21:31 +0100

    extra: test experiment

--- langid.py
+++ langid.py
@@ -13,18 +13,23 @@ df =pd.read_csv('langid.csv', header=0)
 X = df['doc']
 y = df['language']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=44, stratify=y)
-# wasn't working without 'stratify=y', why?
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True, random_state=44)
 
-# part 2
+""" # part 2
 cv = CountVectorizer()
 knn = KNeighborsClassifier()
-pipe = Pipeline(steps=[('cv', cv), ('knn', knn)])
+pipe = Pipeline(steps=[('cv', cv), ('knn', knn)]) """
 
-# part 3
-parameters = {'cv__ngram_range': [(1,1), (1,2), (2,2)], 'knn__n_neighbors': [3, 5, 7], 'knn__metric': ['manhattan', 'cosine']} 
+# extra: test experiment
+cv = CountVectorizer(ngram_range=(1,1))
+knn = KNeighborsClassifier(n_neighbors=3, metric='manhattan')
+p = Pipeline(steps=[('cv', cv), ('knn', knn)])
+cv_scores_train = cross_val_score(p, X_train, y_train, cv=2)
+print('cv_scores mean of train data:{}'.format(np.mean(cv_scores_train)))
+
+""" # part 3
+parameters = {'cv__ngram_range': [(1,1), (1,2), (2,2)], 'knn__n_neighbors': [3, 5, 7], 'knn__metric': ['manhattan', 'cosine', 'euclidean']} 
 # without taking into account crossval, the experiment will run 3*3*2=18 times
-# wasn't working with euclidian distance function, why?
 
 # part 4
 GSCV = GridSearchCV(pipe, parameters, iid=False, cv=10, return_train_score=False, verbose=10)
@@ -35,7 +40,7 @@ print(GSCV.best_params_)
 # Best parameter (CV score=0.744):
 # {'cv__ngram_range': (1, 1), 'knn__metric': 'cosine', 'knn__n_neighbors': 3}
 
-""" # part 5
+# part 5
 cv = CountVectorizer(ngram_range=(1,1))
 knn = KNeighborsClassifier(n_neighbors=3, metric='cosine')
 pipe = Pipeline(steps=[('cv', cv), ('knn', knn)])
